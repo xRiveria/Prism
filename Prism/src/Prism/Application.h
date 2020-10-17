b4e49410809d3fd3457d8a4554b387e59a12218d
Ryan Tan <ryan-wende@outlook.com>
2020-10-17 20:49:56 +0800

    Begin OpenGL Rendering & Create First Triangle

--- Prism/src/Prism/Application.h
+++ Prism/src/Prism/Application.h
@@ -32,6 +32,10 @@ namespace Prism
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
+		unsigned int m_VertexArray;
+		unsigned int m_VertexBuffer;
+		unsigned int m_IndexBuffer;
+
 	private:
 		static Application* s_Instance; 
 	};
