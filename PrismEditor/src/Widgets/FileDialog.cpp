6df689e419401b17055051008176b5932053b45a
Ryan Tan <ryan-wende@outlook.com>
2020-11-28 20:06:50 +0800

    Continue Implementation of File Dialog

--- PrismEditor/src/Widgets/FileDialog.cpp
+++ PrismEditor/src/Widgets/FileDialog.cpp
@@ -1,10 +1,154 @@
-
 #include "FileDialog.h"
 
 namespace Prism
 {
 
+#define OPERATION_NAME (m_DialogOperation == FileDialogOperation_Open) ? "Open" : (m_DialogOperation == FileDialogOperation_Load) ? "Load" : (m_DialogOperation == FileDialogOperation_Save) ? "Save" : "View"
+#define FILTER_NAME
 
 
+	FileDialog::FileDialog(bool standaloneWindow, FileDialogType dialogType, FileDialogOperation dialogOperation, FileDialogFilter dialogFilter)
+	{
+		//Context
+		m_DialogType = dialogType;
+		m_DialogOperation = dialogOperation;
+		m_DialogFilter = dialogFilter;
+		m_Title = OPERATION_NAME;
+		m_IsWindow = standaloneWindow;
+		m_ItemSize = glm::vec2(100.0f, 100.0f);
+		m_IsDirty = true;
+		m_SelectionMade = false;
+		m_CallbackOnItemClicked = nullptr;
+		m_CallbackOnItemDoubleClicked = nullptr;
+		//Navigate Call
+	}
+
+	void FileDialog::SetOperation(const FileDialogOperation& operation)
+	{
+		m_DialogOperation = operation;
+		m_Title = OPERATION_NAME;
+	}
+
+	//Shows the dialog and returns true if a selection was made.
+	bool FileDialog::Show(bool* isVisible, std::string* directory /*= nullptr*/, std::string* filePath /*= nullptr*/)
+	{
+		if (!(isVisible))
+		{
+			m_IsDirty = true; //Set as dirty as things can change until the next time.
+			return false;
+		}
+
+		m_SelectionMade = false;
+		m_IsHoveringItem = false;
+		m_IsHoveringWindow = false;
+
+		ShowTop(isVisible); //Top Menu
+		ShowMiddle(); //Contents of the current directory.
+		ShowBottom(isVisible); //Bottom Menu
+
+		if (m_IsWindow)
+		{
+			DialogUpdateFromDirectory(m_Navigation.m_CurrentPath);
+			m_IsDirty = false;
+		}
+
+		if (m_SelectionMade)
+		{
+			if (directory)
+			{
+				(*directory) = m_Navigation.m_CurrentPath;
+			}
+
+			if (filePath)
+			{
+				(*filePath) = m_Navigation.m_CurrentPath + "/" + std::string(m_InputBox);
+			}
+		}
+
+		EmptyAreaContextMenu();
+
+		return m_SelectionMade;
+	}
+
+	void FileDialog::ShowTop(bool* isVisible)
+	{
+		if (m_IsWindow)
+		{
+			ImGui::SetNextWindowSize(ImVec2(400, 400), ImGuiCond_FirstUseEver); //No persistent save data.
+			ImGui::SetNextWindowSizeConstraints(ImVec2(350, 250), ImVec2(FLT_MAX, FLT_MAX));
+			ImGui::Begin(m_Title.c_str(), isVisible, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoDocking);
+			ImGui::SetWindowFocus();
+		}
+
+		//Directory Navigation Buttons
+		{
+			//Backwards
+			if (ImGui::Button("<"))
+			{
+				m_IsDirty = m_Navigation.BackwardDirectory();
+			}
+
+			ImGui::SameLine();
+
+			//Forwards
+			if (ImGui::Button(">"))
+			{
+				m_IsDirty = m_Navigation.ForwardDirectory();
+			}
+
+			//Individual Directories Button
+			for (uint32_t i = 0; i < m_Navigation.m_PathHierarchy.size(); i++)
+			{
+				ImGui::SameLine();
+				if (ImGui::Button(m_Navigation.m_PathHierarchyLabels[i].c_str()))
+				{
+					m_IsDirty = m_Navigation.Navigate(m_Navigation.m_PathHierarchy[i]);
+				}
+			}
+		}
+
+		//Size Slider
+		const float sliderWidth = 200.0f;
+		ImGui::SameLine(ImGui::GetWindowContentRegionWidth() - sliderWidth);
+		ImGui::PushItemWidth(sliderWidth);
+		
+		const float previousWidth = m_ItemSize.x;
+		ImGui::SliderFloat("FileDialogSlider", &m_ItemSize.x, m_MinimumItemSize, m_MaximumItemSize);
+		m_ItemSize.y += m_ItemSize.x - previousWidth;
+		ImGui::PopItemWidth();
+
+		//Search Filter
+		const float labelWidth = 37.0f;
+		m_SearchFilter.Draw("Filter", ImGui::GetContentRegionAvail().x - labelWidth);
+
+		ImGui::Separator();
+	}
+
+	void FileDialog::ShowMiddle()
+	{
+		//Compute Some Useful Stuff
+		const ImGuiWindow* window = ImGui::GetCurrentWindowRead();
+		const float contentWidth = ImGui::GetContentRegionAvail().x;
+		const float contentHeight = ImGui::GetContentRegionAvail().y - m_BottomOffset;
+		
+		ImGuiContext& context = *GImGui;
+		ImGuiStyle& style = ImGui::GetStyle();
+
+		const float fontHeight = context.FontSize;
+		const float labelHeight = fontHeight;
+		const float textOffset = 3.0f;
+		const float penXMinimum = 0.0f;
+		const float penX = 0.0f;
+		bool newLine = true;
+		m_DisplayedItemCount = 0;
+
+		ImRect rectButton;
+		ImRect rectLabel;
+
+	}
+
+	void FileDialog::ShowBottom(bool* isVisible)
+	{
+	}
 
 }
--- PrismEditor/src/Widgets/FileDialog.h
+++ PrismEditor/src/Widgets/FileDialog.h
@@ -3,6 +3,11 @@
 #include <string>
 #include <vector>
 #include <filesystem>
+#include <chrono>
+#include <imgui/imgui.h>
+#include <imgui/imgui_internal.h>
+#include <functional>
+#include <variant>
 
 namespace Prism
 {
@@ -26,26 +31,70 @@ namespace Prism
 		FileDialogFilter_Model
 	};
 
-	class FileDialogNavigation
+	//============ To Move Out ===================
+	//Drag and Drop
+	enum DragPayloadType
 	{
-	public:
-		bool IsDirectoryValid(const std::string& path)
-		{
-			try
-			{
-				if (std::filesystem::exists(path) && std::filesystem::is_directory(path))
-				{
-					return true;
-				}
-			}
+		DragPayload_Unknown,
+		DragPayload_Texture,
+		DragPayload_Entity,
+		DragPayload_Model,
+		DragPayload_Audio,
+		DragPayload_Script,
+		DragPayload_Material
+	};
 
-			catch (std::filesystem::filesystem_error& caughtError)
+	struct DragDropPayload
+	{
+		typedef std::variant<const char*, unsigned int> DataVariant;
+		DragDropPayload(const DragPayloadType type = DragPayloadType::DragPayload_Unknown, const DataVariant data = nullptr)
+		{
+			this->m_Type = type;
+			this->m_Data = data;
+		}
+
+		DragPayloadType m_Type;
+		DataVariant m_Data;
+		
+	};
+
+	inline static bool IsDirectoryValid(const std::string& path)  //static inline generally works as static, but the inline keyword suggest compiler trying to inline this function.
+	{
+		try
+		{
+			if (std::filesystem::exists(path) && std::filesystem::is_directory(path))
 			{
-				std::string displayedWarning = std::string("Invalid Path: ") + path.c_str();
-				PRISM_EDITOR_WARN(displayedWarning);
+				return true;
 			}
 		}
 
+		catch (std::filesystem::filesystem_error& caughtError)
+		{
+			std::string displayedWarning = std::string("Invalid Path: ") + path.c_str();
+			PRISM_EDITOR_WARN(displayedWarning);
+		}
+	}
+
+	inline static std::string GetFileNameFromFilePath(const std::string& path)
+	{
+		return std::filesystem::path(path).filename().generic_string();
+	}
+
+	static uint32_t g_GlobalID = 0;
+
+	class GenerateGlobalObjectID
+	{
+	public:
+		static uint32_t GenerateID() { return ++g_GlobalID; }  //Return and increment.
+	};
+
+
+	//=============================================
+
+	//Keeps track of directory navigation.
+	class FileDialogNavigation
+	{
+	public:
 		bool Navigate(std::string directory, const bool& updateTrackedHistory = true)
 		{
 			if (!IsDirectoryValid(directory))
@@ -71,8 +120,60 @@ namespace Prism
 			//Update our navigated history if enabled.
 			if (updateTrackedHistory)
 			{
-
+				m_PathHistory.emplace_back(m_CurrentPath);
+				m_PathHistoryIndex++;
 			}
+
+			//Clear Hierarchy
+			m_PathHierarchy.clear();
+			m_PathHierarchyLabels.clear();
+
+			//Is there a slash?
+			std::size_t position = m_CurrentPath.find('/'); 
+
+			//If there are no slashes, there is no nesting, and we are done.
+			if (position == std::string::npos) //std::string::npos is implementation defined index that is always out of bounds of any std::string instance. 
+			{
+				m_PathHierarchy.emplace_back(m_CurrentPath);
+			}
+			else //If there is a slash, get the individual directories between slashes.
+			{
+				std::size_t previousPosition = 0;
+				while (true)
+				{
+					//Lets save everything we have before the slash.
+					m_PathHierarchy.emplace_back(m_CurrentPath.substr(0, position)); //If any slashes are found, it means we have another directory. We save it when the loop returns back here with the updated position.
+
+					//Attempt to find a slash after the one we already found.
+
+					previousPosition = position;
+					position = m_CurrentPath.find('/', position + 1); //Returns npos when the pattern isn't found.
+
+					//If there are no more slashes.
+					if (position == std::string::npos) //If position reaches npos.
+					{
+						//Save the complete path to this directory.
+						m_PathHierarchy.emplace_back(m_CurrentPath); 
+						break;
+					}
+				}
+			}
+
+			//Create a proper looking label to show in the editor for each path. -> To Revamp
+			for (const auto& path : m_PathHierarchy)
+			{
+				position = path.find('/');
+				if (position == std::string::npos) 
+				{
+					m_PathHierarchyLabels.emplace_back(path + " >");
+				}
+				else
+				{
+					m_PathHierarchyLabels.emplace_back(path.substr(path.find_last_of('/') + 1) + " >"); //We use > to indicate that there is a next path.
+				}
+			}
+
+			return true;
 		}
 
 		bool ForwardDirectory()
@@ -106,4 +207,108 @@ namespace Prism
 		int m_PathHistoryIndex = -1;
 	};
 
+	class FileDialogItem
+	{
+	public:
+		FileDialogItem(const std::string& path, const Reference<Texture2D> thumbnail)
+		{
+			m_Path = path;
+			m_Thumbnail = thumbnail;
+			m_ID = GenerateGlobalObjectID::GenerateID();
+			m_IsDirectory = IsDirectoryValid(path);
+			m_Label = GetFileNameFromFilePath(path);
+		}
+
+		const std::string& GetPath() const { return m_Path; }
+		const std::string& GetLabel() const { return m_Label; }
+		const unsigned int& GetID() const { return m_ID; }
+		//const Reference<Texture2D> GetTexture() const { return ;
+		const bool& IsDirectory() const { return m_IsDirectory; }
+		const float& GetTimeSinceLastClickMilliseconds() const { return static_cast<float>(m_TimeSinceLastClick.count()); }
+
+		void Clicked()
+		{
+			const auto timeNow = std::chrono::high_resolution_clock::now();
+			m_TimeSinceLastClick = timeNow - m_LastClickTime;
+			m_LastClickTime = timeNow;
+		}
+
+	private:
+		Reference<Texture2D> m_Thumbnail;
+		unsigned int m_ID;
+		std::string m_Path;
+		std::string m_Label;
+		bool m_IsDirectory;
+		std::chrono::duration<double, std::milli> m_TimeSinceLastClick;
+		std::chrono::time_point<std::chrono::high_resolution_clock> m_LastClickTime;
+	};
+
+	class FileDialog
+	{
+	public:
+		FileDialog(bool standaloneWindow, FileDialogType dialogType, FileDialogOperation dialogOperation, FileDialogFilter dialogFilter);
+
+		//Type & Filter
+		FileDialogType GetType() const { return m_DialogType; }
+		FileDialogFilter GetFilter() const { return m_DialogFilter; }
+		
+		//Operation
+		FileDialogOperation GetOperation() const { return m_DialogOperation; }
+		void SetOperation(const FileDialogOperation& operation);
+
+		//Shows the dialog and returns true if a selection was made.
+		bool Show(bool* isVisible, std::string* directory = nullptr, std::string* filePath = nullptr);
+
+		void SetCallbackOnItemClicked(const std::function<void(const std::string&)>& callback) { m_CallbackOnItemClicked = callback; }
+		void SetCallbackOnItemDoubleClicked(const std::function<void(const std::string&)>& callback) { m_CallbackOnItemDoubleClicked = callback; }
+
+	private:
+		void ShowTop(bool* isVisible); 
+		void ShowMiddle();
+		void ShowBottom(bool* isVisible);
+			
+		//Item Functionality Handling
+		void ItemDrag(FileDialogItem* item) const;
+		void ItemClick(FileDialogItem* item) const;
+		void ItemContextMenu(FileDialogItem* item) const;
+
+		//Misc
+		bool DialogUpdateFromDirectory(const std::string& path);
+		void EmptyAreaContextMenu();
+
+		//Options
+		const bool m_DropShadow = true;
+		const float m_MinimumItemSize = 50.0f;
+		const float m_MaximumItemSize = 200.0f;
+		const glm::vec4 m_ContentBackground = glm::vec4(0.0f, 0.0f, 0.0f, 50.0f);
+
+		//Flags
+		bool m_IsWindow;
+		bool m_SelectionMade;
+		bool m_IsDirty;
+		bool m_IsHoveringItem;
+		bool m_IsHoveringWindow;
+
+		std::string m_Title; //Window Title
+		FileDialogNavigation m_Navigation; //Navigation Context
+		std::string m_InputBox;
+		std::string m_HoveredItemPath;
+		uint32_t m_DisplayedItemCount;
+
+		//Internal
+		mutable unsigned int m_ContextMenuID;
+		mutable DragDropPayload m_DragDropPayload;
+		float m_BottomOffset = 0.0f;
+		FileDialogType m_DialogType;
+		FileDialogOperation m_DialogOperation;
+		FileDialogFilter m_DialogFilter;
+		std::vector<FileDialogItem> m_Filter;
+		glm::vec2 m_ItemSize;
+		ImGuiTextFilter m_SearchFilter;
+		
+		//Callbacks
+		std::function<void(const std::string&)> m_CallbackOnItemClicked;
+		std::function<void(const std::string&)> m_CallbackOnItemDoubleClicked;
+	};
+	
 }
